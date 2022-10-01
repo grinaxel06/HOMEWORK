c10e207bbd84b10c3b2370580c7f82f4eb162b7f
grinaxel06 <grin_axel@inbox.ru>
2022-10-01 16:02:17 +0300

    123

--- src/homework2/H4.java
+++ src/homework2/H4.java
@@ -1,4 +1,20 @@
 package homework2;
 
+import java.util.Scanner;
+
 public class H4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int b = 9;
+        while (N > 0){
+            if (N%10<b && N%10>0){
+                b=N%10;
+            }
+            N/= 10;
+        }
+        System.out.println(b);
+    }
 }
+
+
