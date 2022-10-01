2750a87c68d686d2e92a6de851a0e49826190462
grinaxel06 <grin_axel@inbox.ru>
2022-10-01 15:50:45 +0300

    123

--- src/homework2/H3.java
+++ src/homework2/H3.java
@@ -1,4 +1,21 @@
 package homework2;
 
+import java.util.Scanner;
+
 public class H3 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        if (N <= 14 && N >= 11) {
+            System.out.println(N + " TORTOV");
+        } else {
+            switch (N % 10) {
+                case 1:System.out.println(N + " TORT");
+                case 2, 3, 4 : System.out.println(N + " TORTA");
+                default : System.out.println(N + " TORTOV");
+            }
+        }
+    }
 }
+
