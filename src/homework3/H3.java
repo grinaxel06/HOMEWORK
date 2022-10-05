0cdf7aee892d4b1b3f2275ddc5dc5530a6062bd1
grinaxel06 <grin_axel@inbox.ru>
2022-10-05 18:13:53 +0300

    123

--- src/homework3/H3.java
+++ src/homework3/H3.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class H3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int min = 100000;
+        int min = -1;
         int [] M = new int [in.nextInt()];
         for (int i =0; i< M.length;i++){
            M[i]= in.nextInt();
-            if(M[i]>0 && M[i]<min){
+            if(M[i]>0 && M[i]<min || min < 0){
                 min=M[i];
             }
         }
