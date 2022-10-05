e27981126ef123368ebb6dae7587f39da0c3bab2
grinaxel06 <grin_axel@inbox.ru>
2022-10-05 18:14:25 +0300

    123

--- src/homework3/H5.java
+++ src/homework3/H5.java
@@ -6,11 +6,11 @@ public class H5 {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
         int a;
-        int min= 100000;
+        int min= -1;
         int N = in.nextInt();
         for (int i =0; i<N;i++){
             a= in.nextInt();
-            if (a>0 && a<min){
+            if (a>0 && a<min || min < 0){
                 min=a;
             }
         }
