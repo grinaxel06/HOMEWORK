015afbce754b5ddabdffd9bbc29b114d7ab3fccf
grinaxel06 <grin_axel@inbox.ru>
2022-10-07 17:37:08 +0300

    123

---
+++ src/homework4/MyArrays.java
@@ -0,0 +1,35 @@
+package homework4;
+
+import java.util.Scanner;
+
+public class MyArrays {
+    public static double avgOf4Digit(int N){
+        Scanner in = new Scanner(System.in);
+        double sum=0;
+        double cot = 0;
+        int M[] = new int[N];
+        for(int i =0; i< N;i++){
+            M[i]= in.nextInt();
+            if (M[i]>0 && M[i]/1000>0 && M[i]/1000<10){
+                sum+=M[i];
+                cot++;
+            }
+
+
+        }
+        if (sum>0) {
+            return sum = sum / cot;
+        }
+        else{
+            return -1;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N =in.nextInt();
+        System.out.println(avgOf4Digit(N));
+
+    }
+}
