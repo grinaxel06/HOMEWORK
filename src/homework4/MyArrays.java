495f4ed9becf812f29bbfe9486349b995747f9a1
grinaxel06 <grin_axel@inbox.ru>
2022-10-07 19:17:32 +0300

    123

--- src/homework4/MyArrays.java
+++ src/homework4/MyArrays.java
@@ -3,6 +3,24 @@ package homework4;
 import java.util.Scanner;
 
 public class MyArrays {
+    public static int MinToBegin (int N){
+        int arr[] = new int[N];
+
+        Scanner in = new Scanner(System.in);
+        for (int i =0; i<arr.length-1;i++){
+            arr[i]=in.nextInt();
+        }
+        int min = arr[0];
+        for (int i = 0;i<arr.length-1;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+
+        }
+
+
+        return arr[N];
+    }
     public static double avgOf4Digit(int N){
         Scanner in = new Scanner(System.in);
         double sum=0;
