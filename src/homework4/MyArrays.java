73c8787a2bee8dd9c2c0c4138a2b82301af58787
grinaxel06 <grin_axel@inbox.ru>
2022-10-08 14:31:39 +0300

    123

--- src/homework4/MyArrays.java
+++ src/homework4/MyArrays.java
@@ -3,10 +3,13 @@ package homework4;
 import java.util.Scanner;
 
 public class MyArrays {
-    public static int MinToBegin (int N){
-        int arr[] = new int[N];
-
+    public static int[] MinToBegin (int[] M){
         Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int arr[] = M;
+        int c = 0;
+
+
         for (int i =0; i<arr.length-1;i++){
             arr[i]=in.nextInt();
         }
@@ -14,12 +17,19 @@ public class MyArrays {
         for (int i = 0;i<arr.length-1;i++){
             if(arr[i]<min){
                 min=arr[i];
+                c=i;
+
             }
+        }
+        for (int i =c ;i>0;i--){
+            arr[i]=arr[i-1];
+
 
         }
+        arr[0]=min;
+        return arr;
 
 
-        return arr[N];
     }
     public static double avgOf4Digit(int N){
         Scanner in = new Scanner(System.in);
@@ -47,7 +57,16 @@ public class MyArrays {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N =in.nextInt();
+        int M[]= new int[N];
+        for(int i = 0; N>i;i++){
+            M[i]= in.nextInt();
+        }
+
         System.out.println(avgOf4Digit(N));
+        System.out.println(MinToBegin(M));
+
 
     }
+
+
 }
