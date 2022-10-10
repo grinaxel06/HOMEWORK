632ce1e475fb3e5aa460d65de8924ab293d08931
grinaxel06 <grin_axel@inbox.ru>
2022-10-10 18:30:36 +0300

    123

---
+++ src/homework4/H3.java
@@ -0,0 +1,18 @@
+package homework4;
+
+import java.util.Scanner;
+
+public class H3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int [][] A =new int[n][m] ;
+        for (int i=0;i<n;i++){
+            for(int j=0;i<m;j++){
+
+            }
+        }
+
+    }
+}
