b6f97a5494feb9b87b322f149f41bab43fa59093
grinaxel06 <grin_axel@inbox.ru>
2022-10-11 19:06:56 +0300

    123

--- src/homework4/H3.java
+++ src/homework4/H3.java
@@ -7,36 +7,64 @@ public class H3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int c=0;
-        int [][] A =new int[n][n] ;
-        int j_max = n-1;
-        int i_max = n-1;
-        for (int i=0;i<n;i++){
-            for(int j=0;i<n;j++){
-                if (i==0) {
-                    A[i][j] = c;
-                    c++;
+        int s = 1;
+        int[][] array = new int[n][n];
+        for (int y = 0; y < n; y++) {
+            array[0][y] = s;
+            s++;
+        }
+        for (int x = 1; x < n; x++) {
+            array[x][n - 1] = s;
+            s++;
+        }
+        for (int y = n - 2; y >= 0; y--) {
+            array[n - 1][y] = s;
+            s++;
+        }
+        for (int x = n - 2; x > 0; x--) {
+            array[x][0] = s;
+            s++;
+        }
+        int c = 1;
+        int d = 1;
+        while (s < n * n) {
+            //право
+            while (array[c][d + 1] == 0) {
+                array[c][d] = s;
+                s++;
+                d++;
+            }
+            //лево
+            while (array[c + 1][d] == 0) {
+                array[c][d] = s;
+                s++;
+                c++;
+            }
+            //низ
+            while (array[c][d - 1] == 0) {
+                array[c][d] = s;
+                s++;
+                d--;
+            }
+            //верх
+            while (array[c - 1][d] == 0) {
+                array[c][d] = s;
+                s++;
+                c--;
+            }
+        }
+
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                if (array[x][y] == 0) {
+                    array[x][y] = s;
                 }
-                if(j_max==j){
-                    A[i][j] = c;
-                    c++;
-
-
-                }
-                if (i_max ==i){
-                    A[i][j] = c;
-                    c++;
-
-                }
-
-
             }
         }
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++)
-                System.out.print(A[i][j]+"  " + "\t");
+            for (int j = 0; j < n;j++)
+                System.out.print(array[i][j]+" " + "\t");
             System.out.println();
         }
-
     }
 }
